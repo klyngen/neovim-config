3599b567ff62b743f2e335f31cb2c091fa6e58f8
root <martin@klingenberg.as>
2021-07-10 09:06:55 +0000

    better setup

--- install.sh
+++ install.sh
@@ -1,1 +1,7 @@
-sudo dnf install nodejs ripgrep fzf
+#!/bin/bash
+sudo dnf install nodejs ripgrep  git wget
+mkdir -p ~/.config/nvim/site/autoload
+mkdir ~/.config/nvim/plugged
+wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim ~/.config/nvim/site/autoload/plug.vim
+nvim +PlugInstall +qa
+
