145573a0642e76023adcde9584c8083a64985bb3
Martin Klingenberg <klyngen@alv.no>
2022-04-17 17:08:44 -0400

    Switched from wget to curl

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Install dependencies
-sudo dnf install nodejs ripgrep  git wget pandoc texlive-shceme-medium
+sudo dnf install nodejs ripgrep git pandoc 
 
 # Insall plugged
 mkdir ~/.config/nvim/plugged
 mkdir -p ~/.config/nvim/site/autoload
-wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim ~/.config/nvim/site/autoload/plug.vim
+curl https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -o ~/.config/nvim/site/autoload/plug.vim
 
 # Install pandoc template
 mkdir -p ~/.local/share/pandoc/templates
-wget -O ~/.local/share/pandoc/templates/default.tex -P  https://raw.githubusercontent.com/Wandmalfarbe/pandoc-latex-template/master/eisvogel.tex
+curl https://raw.githubusercontent.com/Wandmalfarbe/pandoc-latex-template/master/eisvogel.tex -o ~/.local/share/pandoc/templates/default.tex  
 
 # Run plugin installation
 nvim +PlugInstall +qa
